dde93c626ef2b7e9af4892a2b9f237aad42db0ee
andreubltzr <andreubltzr11@gmail.com>
2023-09-13 20:00:08 +0200

    added event endpoint

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -1,8 +1,13 @@
 import { NextResponse } from 'next/server';
+import { events } from '@/data/events';
+import { NextApiRequest } from 'next';
 
-export function GET(request, { params }) {
-    console.log(params.id);
-    return NextResponse.json({ message: params.id });
+export function GET(request: NextApiRequest, { params }: {params: {id: number}} ) {
+    const event = events.find(event => event.id === params.id);
+    console.log(event);
+
+    return NextResponse.json(event);
+
 }
 
 export function POST(){}
--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,5 +1,15 @@
 import { NextResponse } from 'next/server';
+import { events } from '@/data/events';
 
 export function GET() {
-    return NextResponse.json({ message: 'hola' });
+    return NextResponse.json(events);
+}
+
+export async function POST(request) {
+    const data = await request.json();
+    console.log(data);
+
+    return NextResponse.json({
+        message: 'Event created.'
+    });
 }
