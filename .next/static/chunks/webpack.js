e64c735b288ec434c3b46d993acf53006f955523
Yumi-Namie <rn.yumi@gmail.com>
2023-09-15 18:45:45 +0200

    Component: ToggleSwitch witch CSS and also label title with subtitle

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f970be253dbff57e"; }
+/******/ 		__webpack_require__.h = function() { return "cde211e29f1558dc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
