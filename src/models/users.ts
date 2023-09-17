af833768df869d9d1f1b2c48e475327be019f8c9
Raul <raul.alhena@gmail.com>
2023-09-18 00:48:34 +0200

    next-auth & bcrypt & @types/bcryptjs installed. Updated models & register page created

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,10 +1,24 @@
 import mongoose, { Schema } from 'mongoose';
 
 const userSchema = new Schema({
-    name: String,
-    surname: String,
-    email: String,
-    password: String,
+    name: {
+        type: String,
+        required: [ true, 'El nombre es requerido' ]
+    },
+    surname: {
+        type: String,
+        required: [ true, 'Apellidos son requeridos' ]
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [ true, 'El email es requerido' ],
+    },
+    password: {
+        type: String,
+        required: [ true, 'El password es requerido' ],
+        select: false
+    }
 });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
