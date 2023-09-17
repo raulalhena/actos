8e370245f0d08c47c133b0e9735b55c5c89bb040
Raul <raul.alhena@gmail.com>
2023-09-17 12:13:35 +0200

    component created and working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { ImageUploader } from '@/components/ImageUploader/ImageUploader';
 
 export default function Home() {
     const a = 'hola';
 
     return (
         <>
-      Hola
+            Hola
         </>
     );
 }
